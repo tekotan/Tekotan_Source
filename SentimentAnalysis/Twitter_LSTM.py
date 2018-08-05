b9c4f0df4dc6f04067e7afa64ff93830c03bd069
Tanish <b.tanish@gmail.com>
2018-08-05 16:02:57 -0700

    minor change

--- SentimentAnalysis/Twitter_LSTM.py
+++ SentimentAnalysis/Twitter_LSTM.py
@@ -4,10 +4,10 @@ from random import randint
 from mysite.settings import STATIC_URL
 
 #Importing Data
-wordsList = np.load('' + STATIC_URL + 'tensorboard/Numpy_Arrays/wordsList.npy')
+wordsList = np.load('../' + STATIC_URL + 'tensorboard/Numpy_Arrays/wordsList.npy')
 wordsList = wordsList.tolist()
 wordsList = [word.decode('UTF-8') for word in wordsList]
-wordVectors = np.load('' + STATIC_URL + 'tensorboard/Numpy_Arrays/wordVectors.npy')
+wordVectors = np.load('../' + STATIC_URL + 'tensorboard/Numpy_Arrays/wordVectors.npy')
 strip_special_chars = re.compile("[^A-Za-z0-9 ]+")
 
 #Helper Functions
@@ -132,4 +132,4 @@ def results():
     return((np.sum(np.equal(x, y))/x.shape))
 sess = tf.InteractiveSession()
 saver = tf.train.Saver()
-saver.restore(sess, tf.train.latest_checkpoint(STATIC_URL +'/tensorboard/RNN2'))
+saver.restore(sess, tf.train.latest_checkpoint('../'+STATIC_URL +'/tensorboard/RNN2'))
