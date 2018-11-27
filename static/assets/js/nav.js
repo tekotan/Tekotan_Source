11725a424fbc3697dbb8b3e2e173a8a048fd1573
Tanish Baranwal <b.tanish@tugun.ai>
2018-11-27 13:33:46 -0800

    Minor Changes

--- static/assets/js/nav.js
+++ static/assets/js/nav.js
@@ -1,13 +1,22 @@
-$(document).ready(function(){
-	$(window).bind('scroll', function() {
-		var navHeight = $('#headermain').height() + 400;
-		if ($(window).scrollTop() > navHeight) {
-			$('.nav-bar').addClass('fixed');
-			$('.nav-1').addClass('toleft');
-		 }
-		else {
-			$('.nav-bar').removeClass('fixed');
-			$('.nav-1').removeClass('toleft');
-		 }
-	});
+$(document).ready(function() {
+  $(window).bind('scroll', function() {
+    var navHeight = $('#headermain').height() + 400;
+    if ($(window).scrollTop() > navHeight) {
+      $('.nav-bar').addClass('fixed');
+      $('.nav-1').addClass('toleft');
+    } else {
+      $('.nav-bar').removeClass('fixed');
+      $('.nav-1').removeClass('toleft');
+    }
+  });
+  $(".toggle").click(function() {
+    $('.links').slideToggle();
+  });
+  $(window).resize(function() {
+    if ($(window).width() > 768) {
+      $('.links').show();
+    } else {
+      $('.links').hide();
+    }
+  });
 });
